ab379a57c50d134bd4b9228b6ea05aa4034a6da1
DanAtKW <daniel.chandy@kilowott.com>
2026-03-10 13:14:56 +0530

    merged dashboard to client

--- src/CompanyOuting.App/obj/Debug/net10.0/CompanyOuting.App.AssemblyInfo.cs
+++ src/CompanyOuting.App/obj/Debug/net10.0/CompanyOuting.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyOuting.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d75ad517e6d5479f8cb111927c152ed82ec7b3c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e9fafc254654c52dd70e3807061452e6a37a924")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyOuting.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyOuting.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
